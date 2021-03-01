ff6005f61e64c6309fbb3c44959459e281fe955f
VitorSancho <vitor.sancho07@gmail.com>
2021-02-28 22:11:34 -0300

    Add files via upload

--- gerenciamento_db.py
+++ gerenciamento_db.py
@@ -5,14 +5,21 @@ connection = sqlite3.connect('ProjetoAcoesTwitter.db')
 # cursor permite execução de comandos SQL
 Cursor = connection.cursor()
 
+campos_negociacoes = "id integer primary key autoincrement,stock varchar(20),data_id int, Date DATETIME, Close numeric, Volume bigint, FOREIGN KEY (stock) REFERENCES tb_stocks (stock)"
+campos_tweet = "id integer primary key autoincrement,stock varchar(20), id_tweets int, horario_tweet timestamp,conteudo varchar, FOREIGN KEY (stock) REFERENCES tb_stocks (stock)"
+campos_stock = "stock varchar(20), empresa varchar(20), país varchar(20), PRIMARY KEY (stock)"
 
-def cria_tabela(nome_tabela, cursor_database):
+
+def cria_tabela(nome_tabela, cursor_database, campos):
     cursor_database.execute(
-        f'CREATE TABLE if not exists tb_{nome_tabela} (stock text,data_id text, Date DATETIME, Close numeric, Volume bigint)')
+        f'CREATE TABLE if not exists tb_{nome_tabela} ({campos})')
+    # cursor_database.execute(
+    #     f'CREATE TABLE if not exists tb_{nome_tabela} ({campos})')
+    print(f'Tabela tb_{nome_tabela} criada!')
 
 
 def deleta_tabela(nome_tabela, cursor_database):
-    cursor_database.execute(f'DROP TABLE tb_{nome_tabela}')
+    cursor_database.execute(f'DROP TABLE if exists tb_{nome_tabela}')
     connection.commit()
 
 
@@ -44,16 +51,40 @@ def apaga_conjunto_de_tabelas(iteravel, cursor_database):
     print(f'Tabelas foram deletadas de acordo com {tipo}!')
 
 
-STOCKS = {"USA": ['GOOGL', 'AMZN', 'MSFT', 'TSLA', 'AAPL'],
-          "BR": ['ITSA4', 'PETR4', 'VALE3', 'WEGE3']}
+def insere_conjunto_de_dados(conjunto):
+    Cursor.execute(
+        f'INSERT INTO tb_stocks (stock,empresa,país) values {conjunto}')
+    connection.commit()
 
-negociacao_STOCKS = {"USA": ['negociacao_GOOGL', 'negociacao_AMZN', 'negociacao_MSFT', 'negociacao_TSLA', 'negociacao_AAPL'],
-                     "BR": ['negociacao_ITSA4', 'negociacao_PETR4', 'negociacao_VALE3', 'negociacao_WEGE3']}
+    Cursor.execute(
+        'CREATE TABLE if not exists tb_stocks (stock varchar(6) primary key,empresa varchar(15), país varchar(10))')
 
-vale = ['negociacao_googl']
-cria_conjunto_de_tabelas(vale, Cursor)
-# apaga_conjunto_de_tabelas(vale, Cursor)
+# STOCKS = {"USA": ['tweet_GOOGL', 'tweet_AMZN', 'tweet_MSFT', 'tweet_TSLA', 'tweet_AAPL'],
+#           "BR": ['tweet_ITSA4', 'tweet_PETR4', 'tweet_VALE3', 'tweet_WEGE3']}
+#
+# STOCKS = {"USA": ['GOOGL', 'AMZN', 'MSFT', 'TSLA', 'AAPL'],
+#           "BR": ['ITSA4', 'PETR4', 'VALE3', 'WEGE3']}
+#
+# negociacao_STOCKS = {"USA": ['negociacao_GOOGL', 'negociacao_AMZN', 'negociacao_MSFT', 'negociacao_TSLA', 'negociacao_AAPL'],
+#                      "BR": ['negociacao_ITSA4', 'negociacao_PETR4', 'negociacao_VALE3', 'negociacao_WEGE3']}
 
-# Cursor.execute(
-#     'insert into tb_negociacao_GOOGL values ("vvs","cccss","2020-02-02","456.23","3458325082")')
+
+STOCKS = (("GOOGL", "Google", "USA"), ("AMZN", "Amazon", "USA"), ("MSFT", "Microsoft", "USA"),
+          ("TSLA", "Tesla", "USA"), ("AAPL", "Apple",
+                                     "USA"), ("ITSA4", "Itaúsa", "Brasil"),
+          ("PETR4", "Petrobras", "Brasil"), ("VALE3",
+                                             "Vale", "Brasil"), ("WEGE3", "Weg", "Brasil"))
+
+# [insere_conjunto_de_dados(stock) for stock in STOCKS]
+
+# cursor_database.execute('CREATE INDEX indice_stock ON table_name (stock)')
+lista = ['stocks', 'negociacoes', 'tweet']
+apaga_conjunto_de_tabelas(lista, Cursor)
+
+
+# Cursor.execute('delete from tb_tweet')
 # connection.commit()
+
+cria_tabela('stocks', Cursor, campos_stock)
+cria_tabela("tweet", Cursor, campos_tweet)
+cria_tabela("negociacoes", Cursor, campos_negociacoes)
--- twitter.py
+++ twitter.py
@@ -8,7 +8,7 @@ import sqlalchemy
 def data_from_tweet(tweet, stock):
     return pd.DataFrame({
         "stock": [stock],
-        "id_tweet": [tweet.id_str],
+        "id_tweets": [tweet.id_str],
         "horario_tweet": [tweet.created_at.strftime("%Y-%m-%d %H:%M:%S")],
         "conteudo": [tweet.text]})
 
@@ -31,34 +31,37 @@ api = tweepy.API(auth)
 # result = api.search(q="itsa4")
 STOCKS = {"USA": ['GOOGL', 'AMZN', 'MSFT', 'TSLA', 'AAPL'],
           "BR": ['ITSA4', 'PETR4', 'VALE3', 'WEGE3']}
-max_tweets = 5
+max_tweets = 500
 
 for pais in STOCKS.items():
-    lista_de_acoes = pais[1]
-    for ticker_acao in lista_de_acoes:
+    lista_de_stocks = pais[1]
+    for stock in lista_de_stocks:
 
-        if ticker_acao == 'GOOGL':
-            query = ticker_acao+' -GOOGLE'
+        if stock == 'GOOGL':
+            query = stock+' -GOOGLE'
 
-        db_query = f"SELECT MAX(cast(id_tweet as bigint)) as max_id from tb_{ticker_acao}"
+        db_query = f"SELECT MAX(cast(id_tweet as bigint)) as max_id from tb_tweet where stock = {stock}"
         try:
             max_id = pd.read_sql_query(db_query, conexao)['max_id'].values[0]
         except:
             max_id = 0
 
-        query = ticker_acao
+        query = stock
         searched_tweets = [status for status in tweepy.Cursor(
             api.search, q=query, since_id=max_id+1).items(max_tweets)]
 
         quantidade_de_tweets = 0
         try:
-            data = pd.concat([data_from_tweet(i, ticker_acao)
+            data = pd.concat([data_from_tweet(i, stock)
                               for i in searched_tweets])
-            data.to_sql(f"tb_{ticker_acao}", conexao,
+
+            data.to_sql(f"tb_tweet", conexao,
                         if_exists="append", index=False)
+
             quantidade_de_tweets = data.shape[0]
+
             print(
-                f"Foram adicionados {quantidade_de_tweets} dados a tabela tb_{ticker_acao}")
+                f"Foram adicionados {quantidade_de_tweets} tweets sobre {stock} a tabela tb_tweet")
 
         except ValueError:
-            print(f"Não há novos tweets sobre {ticker_acao}")
+            print(f"Não há novos tweets sobre {stock}")
